240dd51b309edd73c5cd1ca5f4f4f6aa9f2a584c
ChloeLL <chloell@protonmail.ch>
2022-01-06 06:08:21 -0700

    Add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  resources :users, only: [:show, :edit]
   get '/register', to: 'users#new'
   get '/login', to: 'users#login_form'
+  post '/login', to: 'users#create'
   resources :resources, only: :index
 end
