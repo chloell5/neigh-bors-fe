613fb89432ddc7fa92b5424f3690a5c31b4ab07d
ChloeLL <chloell@protonmail.ch>
2022-01-06 06:08:50 -0700

    Add show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    redirect_to dashboard_path
+    user = User.create(user_params)
+
+    redirect_to user_path(user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :address, :email, :password, :phone, :horses, :needs, :needs_text, :trailer, :sharing)
   end
 end
