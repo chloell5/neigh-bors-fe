ee75eae8ef2b3864adf7abe5253f759434b02e26
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-01-06 14:38:20 -0700

    Oauth routing & other setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
   get '/register', to: 'users#new'
-  get '/login', to: 'users#login_form'
+  #get '/login', to: 'users#login_form'
   resources :resources, only: :index
 end
