25a549751199ab9bc97e4d7e9ac938798db70a41
ChloeLL <chloell@protonmail.ch>
2022-01-08 13:17:22 -0700

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get '/register', to: 'users#new'
+
+  get '/register', to: 'farms#new'
+  post '/register', to: 'farms#create'
+  get '/dashboard', to: 'farms#show'
   #get '/login', to: 'users#login_form'
+  resources :farms, only: [:show, :edit]
   resources :resources, only: :index
 
   get 'texts', to: 'texts#index'
