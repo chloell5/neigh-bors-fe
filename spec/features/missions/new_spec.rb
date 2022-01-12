53f79ad0afc92682ee008272d437c532f39d0cb1
ChloeLL <chloell@protonmail.ch>
2022-01-12 16:33:20 -0700

    Test new mission

--- spec/features/missions/new_spec.rb
+++ spec/features/missions/new_spec.rb
@@ -3,13 +3,28 @@ require 'rails_helper'
 RSpec.describe 'Mission New' do
   before do
     @user = UserFacade.user_by_email('mdodson55@gmail.com')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     @farm = FarmFacade.find_farm(@user.id)
   end
   it 'has farm information' do
     visit missions_new_path
-    save_and_open_page
+
+    expect(page).to have_content(@farm.name)
+    expect(page).to have_content(@farm.address)
+    expect(page).to have_content(@farm.animals)
+
+    expect(page).to have_content(@user.phone)
+    expect(page).to have_content(@user.email)
+    expect(page).to have_content(@user.trailer_capacity)
   end
 
-  it 'has link to edit page'
+  it 'has link to edit page' do
+    visit missions_new_path
+
+    click_button 'Edit'
+save_and_open_page
+    expect(page).to have_current_path(edit_farm_path)
+  end
+
   it 'submits when clicked'
 end
