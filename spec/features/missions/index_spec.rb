c31061a6525f24e5876f7648e10b1112e8af7560
Philip Medlin <phmedlin@gmail.com>
2022-01-14 09:18:58 -0700

    Fix feat missions index

--- spec/features/missions/index_spec.rb
+++ spec/features/missions/index_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Mission Index' do
-  # Should probably fix all the hardcoding
   before do
-    @user = UserFacade.user_by_email('mdodson55@gmail.com')
+    @user = UserFacade.user_by_email('sallybrown22@gmail.com')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
 
   it 'shows the name of the farm for each rescue' do
     visit missions_path
 
-    expect(page).to have_content('Didi\'s Horse Retreat')
+    expect(page).to have_content('Didi')
   end
 
   it 'shows total number of horses' do
@@ -22,6 +21,6 @@ RSpec.describe 'Mission Index' do
   it 'shows distance to farm' do
     visit missions_path
 
-    expect(page).to have_content('0 miles away')
+    expect(page).to have_content('1.33 miles away')
   end
 end
